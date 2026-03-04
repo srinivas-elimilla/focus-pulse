c01b48ca0b5399cfad591d0a72ed7e3a12616cb9
srinivas-elimilla <srinivaselimilla1@gmail.com>
2026-03-05 04:25:32 +0530

    fix: tray icon path for production

--- electron/tray.js
+++ electron/tray.js
@@ -5,7 +5,7 @@ const store = require("./store");
 let tray = null;
 
 function createTray(createOverlay, restartScheduler) {
-  tray = new Tray(path.join(__dirname, "../build/icon.ico"));
+  tray = new Tray(path.join(__dirname, "timer.png"));
 
   const contextMenu = Menu.buildFromTemplate([
     {
